434ceca00c3ba6f95d4e0cf030cff52e7c0b698f
Luís Felipe Simões <luis.felipe-simoes@outlook.com>
2025-11-09 02:56:15 +0000

    refactor: Remove 'use client' directive from CourseForm and CourseFormContainer components

--- src/components/features/course/CourseForm.tsx
+++ src/components/features/course/CourseForm.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Button from '@/components/ui/Button';
 import Form from '@/components/ui/Form';
 import Input from '@/components/ui/Input';
--- src/containers/course/CourseFormContainer.tsx
+++ src/containers/course/CourseFormContainer.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import CourseForm, {
   CourseFormData,
 } from '@/components/features/course/CourseForm';
